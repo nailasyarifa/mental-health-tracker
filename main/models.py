2c1df2aaf80712e9843a480de152fd2c89dadd4f
nailasyarifa <nailasyarifa05@gmail.com>
2024-09-11 14:51:54 +0700

    coba push pws otomatis

--- main/models.py
+++ main/models.py
@@ -1,9 +1,9 @@
 from django.db import models
+import uuid
 
 # Create your models here.
-from django.db import models
-
 class MoodEntry(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False) #tambahan tutorial2
     mood = models.CharField(max_length=255)
     time = models.DateField(auto_now_add=True)
     feelings = models.TextField()
@@ -12,3 +12,4 @@ class MoodEntry(models.Model):
     @property
     def is_mood_strong(self):
         return self.mood_intensity > 5
+        
